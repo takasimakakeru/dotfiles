0e3062d34ae52d7280b9f7cd545645fe276f067b
takasimakakeru <kyunpangoogle@outlook.jp>
2026-04-13 23:00:32 +0000

    poop

--- init.lua
+++ init.lua
@@ -31,12 +31,17 @@ require("lazy").setup({
   "nvim-treesitter/nvim-treesitter",
   "windwp/nvim-autopairs",
   "windwp/nvim-ts-autotag",
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+
+  -- Telescope を 0.1.8 に固定（0.9.5対応）
+  { "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" } },
+  
   { "nvim-tree/nvim-tree.lua", dependencies = "nvim-tree/nvim-web-devicons" },
 
+  -- LSP関連も固定
   { "neovim/nvim-lspconfig", tag = "v0.1.8" },
   { "williamboman/mason.nvim" },
   { "williamboman/mason-lspconfig.nvim", version = "1.3.0" },
+  
   "hrsh7th/nvim-cmp",
   "hrsh7th/cmp-nvim-lsp",
 })
