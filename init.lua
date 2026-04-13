fdb946eaef2f677cdb529bef273d39ce5689e7ac
takasimakakeru <kyunpangoogle@outlook.jp>
2026-04-13 04:48:32 +0000

    poop

--- init.lua
+++ init.lua
@@ -100,8 +100,8 @@ require("mason-lspconfig").setup({
     ensure_installed = { "html" }
 })
 
--- v0.11以降のLSP設定
-vim.lsp.config('html', {
+-- nvim-lspconfig を使った標準的な書き方
+require('lspconfig').html.setup({
     capabilities = require('cmp_nvim_lsp').default_capabilities()
 })
 
